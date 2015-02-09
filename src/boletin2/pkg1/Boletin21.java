10ad511ef5cca77285838816c413ecf62a8c8f9e
rfernandezgonzalez <rfernandezgonzalez@eniac04.danielcastelao.org>
2015-02-09 09:14:12 +0100

    A trabajar, Souto!!!

---
+++ src/boletin2/pkg1/Boletin21.java
@@ -0,0 +1,22 @@
+package boletin2.pkg1;
+
+import java.util.Scanner;
+
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        
+        float base, altura;
+
+        System.out.print("Introduzca la base del triangulo: ");
+        Scanner dato = new Scanner(System.in);
+        base = dato.nextFloat();
+
+        System.out.print("Introduzca la altura del triangulo: ");
+        altura = dato.nextFloat();
+
+        System.out.println("Área: " + (base * altura / 2) + "u");
+
+    }
+
+}
